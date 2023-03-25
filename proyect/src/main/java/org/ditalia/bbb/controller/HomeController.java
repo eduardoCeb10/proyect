9e978343b7b86b18abc520d70631a39bac91fa61
eduardo <spartanceballos@gmail.com>
2023-03-24 22:17:09 -0600

    Se modifico la tarjeta de destacados

--- proyect/src/main/java/org/ditalia/bbb/controller/HomeController.java
+++ proyect/src/main/java/org/ditalia/bbb/controller/HomeController.java
@@ -2,19 +2,26 @@ package org.ditalia.bbb.controller;
 
 import java.util.LinkedList;
 import java.util.List;
-
 import org.ditalia.bbb.entity.Vestido;
 import org.ditalia.bbb.service.IntServiceVestido;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
 	
 	@Autowired
 	private IntServiceVestido serviceVestido;
+	/*
+	 @Autowired
+	 private IntServiceUsuario serviceUsuario;
+	 @Autowired
+	private PasswordEncoder passwordEncoder;
+	 */
 	
 	@GetMapping("/")
 	public String mostrarIndex(Model model) {
@@ -27,5 +34,42 @@ public class HomeController {
 		}
 		model.addAttribute("vestidos", vestidos);
 		return "home";
+	}
+	
+	@GetMapping("/signup")
+	public String signup() {
+		return "formRegistro";
+	}
+	
+	/*@PostMapping("/guardar")
+	public String guardar(Usuario usuario) {
+		usuario.setEstatus(1);
+		//usuario.setPassword("{noop}"+usuario.getPassword());
+		String texto = usuario.getPassword();
+		String encriptado = passwordEncoder.encode(texto);
+		usuario.setPassword(encriptado);
+		Perfil per = new Perfil();
+		per.setId(3);
+		usuario.agregar(per);
+		serviceUsuario.agregar(usuario);
+		System.out.println(usuario);
+		return "home";
+	}*/
+	
+	@GetMapping("/acerca")
+	public String acerca() {
+		return "acerca";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "formLogin";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request) {
+		SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+		logoutHandler.logout(request, null, null);
+		return "redirect:/";
 	}
 }
